7a96668cd09c5e76f01c7f387e9dcceb24a2f480
hacker65536 <s.hacker65536@gmail.com>
2017-02-06 08:27:15 +0000

    Add tamplate

---
+++ tmp/variables.tf
@@ -0,0 +1,13 @@
+variable "profile" {
+  description = "AWS_PROFILE"
+  default     = "default"
+}
+
+variable "region" {
+  description = "default region"
+  default     = "us-west-2"
+}
+
+variable "tags" {
+  default = {}
+}
